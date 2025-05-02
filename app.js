365bec81a2373d0275ce65a7617697888e626983
Alejandro De Yavorsky <aledeyavorsky@gmail.com>
2025-05-02 10:04:48 +0000

    Traffic light

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
+const setTrafficLight = () =>{
 
+    document.getElementById("colorHigh").innerHTML = `🟢`;
+    document.getElementById("colorLow").innerHTML = `⚪`;ö
+    
+      setTimeout(() => {
+        document.getElementById("colorHigh").innerHTML = `⚪`;
+        document.getElementById("colorMiddle").innerHTML = `🟡`;
+      }, 3000);
+    
+      setTimeout(() => {
+        document.getElementById("colorMiddle").innerHTML = `⚪`;
+        document.getElementById("colorLow").innerHTML = `🔴`;
+      }, 6000);
+    
+      setTimeout(setTrafficLight, 10000);
+    
+    }
